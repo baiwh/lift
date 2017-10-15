47e4656ed3004bdb0e4ff0a883635ba412fa7c3b
baiwh <471974983@qq.com>
2017-10-16 00:40:34 +0800

    增加了对勾
    1对勾的css
    2调整了报错的位置
    3验证密码是否一致

--- script/sign.js
+++ script/sign.js
@@ -1,6 +1,3 @@
-
-
-
 $(function () {
     // 注册登录切换
     $("#li-signUp").click(function () {
@@ -22,9 +19,11 @@ $(function () {
         if (inputValue==""){
             info.html(tip+"不能为空");
             return;
+        }else {
+            info.html("√").attr('class','pass');
         }
     }
-
+    // 邮箱
     $("#email").blur(function () {
         // 判空
         checkNull("email","emailInfo","邮箱");
@@ -35,21 +34,37 @@ $(function () {
         if (!pattern.test(emailVal)){
             info.html("邮箱格式不正确");
         }else {
-            info.html("");
+            // info.html("√").attr('class','pass');
         }
         // 查重
     })
+    // 密码
     $("#pwd").blur(function () {
         // 判空
         checkNull("pwd","pwdInfo","密码");
         // 强弱
+    })
+    // 重复密码
+    $("#pwd2").blur(function () {
+        // 判空
+        checkNull("pwd2","pwd2Info","重复密码");
+        // 一致
+        var password=$("#pwd").val();
+        var password2=$("#pwd2").val();
+        if (password2!=""){
+            if (password!=password2){
+                $("#pwd2Info").html("两次密码不一致");
+            }else {
+                $("#pwd2Info").html("√").attr('class','pass');
+            }
+        }
 
     })
-    $("#pwd2").blur(function () {
-        checkNull("pwd2","pwd2Info","重复密码");
-    })
+    // 验证码
     $("#verifyCode").blur(function () {
+        // 判空
         checkNull("verifyCode","verifyInfo","验证码");
+        // 对错
     })
 
 
