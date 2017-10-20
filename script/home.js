94140c6591cd25f185b47d3c2ad92a7977811bcf
baiwh <471974983@qq.com>
2017-10-20 16:01:09 +0800

    修复了动态添加了很多HTML的bug
    修复：通过js动态添加带有事件的HTML。事件绑定失效。

--- script/home.js
+++ script/home.js
@@ -43,12 +43,14 @@ $(function () {
     }
 
     // 添加新任务
-    $("#add").click(function () {
+    $('body').on('click', '#add', function () {
+    // $("#add").click(function () {
         addElementDiv('list-box');
     })
 
     // 划掉项目
-    $(".items").click(function () {
+    $('body').on('click', '.items', function () {
+    // $(".items").click(function () {
         var items = $(this).find("input[type=checkbox]");
         if (items.is(":checked")) {
             items.attr("checked", false);
@@ -64,12 +66,14 @@ $(function () {
     })
 
     // 添加新的div。点击切换input
-    $(".listSpan").click(function () {
+    $('body').on('click', '.listSpan', function () {
+    // $(".listSpan").click(function () {
         $(this).hide();
         $(this).siblings().show();
     })
     // 光标离开input。切换回span
-    $(".listInput").blur(function () {
+    $('body').on('blur', '.listInput', function () {
+    // $(".listInput").blur(function () {
         $(this).hide();
         $(this).siblings().show();
         // 获取标题
@@ -78,17 +82,16 @@ $(function () {
         if (titInput != "") {
             $(this).parent().find("span").html(titInput);
         }
-
         // 获取日期
         var datInput = $(".day").find("input").val();
         if (datInput != "") {
             $(this).parent().find("span").html(datInput);
         }
-
-
     })
+
     // 输入的同时详细列表改变信息
-    $(".listInput").keyup(function () {
+    $('body').on('keyup', '.listInput', function () {
+    // $(".listInput").keyup(function () {
         // 获取标题
         var titInput = $(".title").find("input").val();
         // 把标题赋给h2
@@ -99,7 +102,8 @@ $(function () {
     })
 
     // 标签悬浮窗的显示和隐藏
-    $(".stateBar span").click(function () {
+    $('body').on('click', '.stateBar span', function () {
+    // $(".stateBar span").click(function () {
         // 获取他的悬浮标签
         var tag=$(this).parent().next("div");
         if(tag.is(":hidden")){
@@ -111,7 +115,8 @@ $(function () {
 
     // 点击悬浮窗内的标签。替换悬浮窗外的
     // 当它被点击
-    $(".allTag span").click(function () {
+    $('body').on('click', '.allTag span', function () {
+    // $(".allTag span").click(function () {
         // 获取悬浮窗内被点击的html
         var newTag=$(this).html();
         // 获取要替换的那个tag
@@ -126,7 +131,8 @@ $(function () {
 
 
     // 被选中的小列表加上效果
-    $(".list").click(function () {
+    $('body').on('click', '.list', function () {
+    // $(".list").click(function () {
         $(this).addClass("choose");
         $(this).siblings().removeClass("choose");
     })
