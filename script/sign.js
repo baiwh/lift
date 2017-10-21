af2439bd2ece3ae64c622e2287d8a5e4ad668ccb
baiwh <471974983@qq.com>
2017-10-21 15:37:06 +0800

    调整了注册页的密码等级标签的效果

--- script/sign.js
+++ script/sign.js
@@ -78,19 +78,28 @@ $(function () {
             }
             if(streng == 1) {
                 // 弱
+                // 第一格变色。除他自己以外的统计元素无色
                 s1.attr("class", "red").siblings("span").attr("class", "");
-                tab.attr("class", "change1");
+                // 改变标签位置和颜色
+                tab.attr("class", "change1").animate({left:"-105px"});
             } else if (streng == 2) {
                 // 中
+                // 第一格变色
                 s1.attr("class", "orange");
+                // 第二格变色。他的下一格无色
                 s2.attr("class", "orange").next("span").attr("class", "");
-                tab.attr("class", "change2");
+                // 改变标签位置和颜色
+                tab.attr("class", "change2").animate({left:"-70px"});
             } else if (streng == 3) {
                 // 强
+                // 第一格变色
                 s1.attr("class", "green");
+                // 第二格变色
                 s2.attr("class", "green");
+                // 第三格变色
                 s3.attr("class", "green");
-                tab.attr("class", "change3");
+                // 改变标签位置和颜色
+                tab.attr("class", "change3").animate({left:"-35px"});
             }
         }
 
