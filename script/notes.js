531ca296c37f201e690e60867f5de5a58e4fe151
baiwh <471974983@qq.com>
2017-10-28 00:37:43 +0800

    便签页
    1、时间和便签
    2、鼠标移入移出垃圾桶效果
    3、鼠标离开输入框获取当前时间
    4、垃圾桶的点击事件
    5、鼠标点击便签的效果

--- script/notes.js
+++ script/notes.js
@@ -1,3 +1,49 @@
 /**
  * Created by 白 on 2017/10/26.
  */
+$(function () {
+
+    // 鼠标移入便签范围。显示垃圾桶
+    $("body").on("mouseover",".note",function () {
+        $(this).find(".delNote").show();
+    })
+
+    // 鼠标移出便签范围。隐藏垃圾桶
+    $("body").on("mouseout",".note",function () {
+        $(this).find(".delNote").hide();
+    })
+
+    // 鼠标离开输入框
+    $("body").on("blur",".noteInput",function () {
+        var noteInput=$(this).val();
+        // 获取当前时间
+        var date=new Date();
+        var hour=date.getHours();
+        var minute=date.getMinutes();
+        var time=""+hour+"："+minute;
+        $(".choose").siblings(".hour").find(".theHour").html(time);
+
+    })
+
+    // 点击垃圾桶就删除他爸爸
+    $("body").on("click",".delNote",function () {
+        $(this).parent().remove();
+    })
+
+    // 点击加号。添加新的
+    $("body").on("click","#add",function () {
+        $(".contents").prepend("<div class=\"note newNote\"><input type=\"hidden\" class=\"noteId\"><div class=\"notes\"><textarea name=\"\" class=\"noteInput\"></textarea></div><img class=\"delNote\" src=\"icon/del.png\" alt=\"\"></div>");
+        if($(".newNote").is(":hidden")){
+            $(".newNote").slideDown();
+        }
+    })
+
+    // 鼠标点击某个便签
+    $("body").on("click",".noteInput",function (){
+        // 添加选中效果
+        $(this).parent().addClass("choose");
+        $(this).parent().parent().siblings().find(".notes").removeClass("choose");
+    })
+
+
+})
